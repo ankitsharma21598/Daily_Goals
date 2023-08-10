934ae017d10010133119898c0487e3d84683f5c1
My java <ankit.sharma21598@gmail.com>
2023-08-10 11:23:47 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <>
+      <nav>Get ready to complete your goals</nav>
+      <div className="header">
+        <Link to="/" >Home</Link>
+        <Link to="/about" >About</Link>
+        <Link to="/contact" >Contact</Link>
+      </div>
+    </>
+  );
+};
+
+export default Header;
